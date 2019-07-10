7618c84dc0fa0650afecb9c360654d0477d8936c
Edward P. Kelly <ed.kelly@nbcuni.com>
2019-07-10 14:20:06 -0400

    Adding clean webpack plugin and cleaning up single quotes in configs

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require("path");
-const common = require('./webpack.common');
-const merge = require('webpack-merge');
+const common = require("./webpack.common");
+const merge = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
     mode: "production",
     output: {
         filename: "[name].[contentHash]-bundle.js",
         path: path.resolve(__dirname, "dist")
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 });
