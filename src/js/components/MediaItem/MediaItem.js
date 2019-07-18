4eb2dafdbe9ea1684df2383d0ebd45d73bb55436
Edward P. Kelly <ed.kelly@nbcuni.com>
2019-07-18 18:45:36 -0400

    Adding header to media item Bootstrap cards

--- src/js/components/MediaItem/MediaItem.js
+++ src/js/components/MediaItem/MediaItem.js
@@ -19,11 +19,12 @@ const MediaItem = props => {
     return (
         <div className="col-lg-4 col-md-6">
             <div className="card mb-4 shadow-sm">
+                <h5 className="card-header text-center">{displayName}</h5>
                 <img className="card-img-top" src={imgUrl} alt={displayName} />
                 <div className="card-body">
                     {
                         (userAuthenticated !== AuthConstants.NOT_AUTHENTICATED) ?
-                            <div class="d-flex justify-content-between align-items-center">
+                            <div className="d-flex justify-content-between align-items-center">
                                 <small>
                                     {
                                         callsigns.length ? 'Choose a location:' : null
